4a6b63026aed4549d31dddb64c674f2c7e7160a3
Gavin Brown <55294040+gavinbrown025@users.noreply.github.com>
2021-03-03 18:28:56 -0500

    time bar and scrubber completed

---
+++ src/components/Playlist.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="playlist">
+    <div
+      v-for="song in songs"
+      :key="song.id"
+      @click="play(song)"
+      :class="(song.preview == current.preview) ? 'song playing' : 'song' "
+    >
+      <p>{{song.artist.name}} - {{song.title}}</p>
+      <span>{{getCurrentTime(song.duration)}}</span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "playlist",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
